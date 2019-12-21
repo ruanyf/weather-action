35036fabf99d4d4c328cd6587471e3d4e925876f
ruanyf <yifeng.ruan@gmail.com>
2019-12-21 22:07:24 +0800

    refactor: edit bash script

--- weather.sh
+++ weather.sh
@@ -2,8 +2,13 @@
 
 set -eux
 
+CITY=Shanghai
+LANGUAGE="zh-CN"
+UNIT=m
+UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
+
 curl \
-  -H "Accept-Language: zh-CN" \
-  -H "User-Agent: Mozilla / 5.0（Windows NT 10.0; Win64; x64）AppleWebKit / 537.36（KHTML，如Gecko）Chrome / 79.0.3945.88 Safari / 537.36" \
+  -H "Accept-Language: $LANGUAGE" \
+  -H "User-Agent: $UA" \
   -o result.html \
-  wttr.in/Shanghai
+  wttr.in/$CITY?$UNIT
