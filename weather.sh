6688096b223c3c1d7da3e65ad8d6ca89a275899b
Sylar <ys_yinqin@outlook.com>
2021-02-19 11:08:45 +0800

    Update weather.sh

--- weather.sh
+++ weather.sh
@@ -2,7 +2,7 @@
 
 set -eux
 
-CITY=Shanghai
+CITY=wuhan
 LANGUAGE="zh-CN"
 UNIT=m
 UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
